abb0e76ae0b3be2b21b2b9c15eb32c390e6caea3
Roy Bosire <roy.bosire62@gmail.com>
2025-09-28 19:53:05 +0000

    Make sure the following Error has been resolved
    Image with src "https:

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -8,7 +8,7 @@ export function Logo() {
       alt={`${SITE_CONFIG.name} Logo`}
       width={160}
       height={40}
-      className="h-auto w-auto object-contain"
+      className="h-10 w-auto"
       priority
     />
   );
