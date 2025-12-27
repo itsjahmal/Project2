7069f212943c625513972c926c972461c4603363
Roy Bosire <roy.bosire62@gmail.com>
2025-12-27 18:32:44 +0000

    I want the Images to be served from their URLs

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,7 +4,7 @@ import { SITE_CONFIG } from '@/lib/config';
 export function Logo() {
   return (
     <Image
-      src="/logo.png"
+      src="https://i.imgur.com/kpvUUgj.png"
       alt={`${SITE_CONFIG.name} Logo`}
       width={160}
       height={40}
